98d0d60a7100941dbfae30add98eaf6554c1142f
francescofalcone05 <ciccio_97@live.it>
2024-05-27 18:06:16 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+  <AppHeader />
+
+  <AppMain />
+
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,57 @@
+<script>
+
+export default {
+    components: {
+
+    },
+
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <header>
+        <div class="logo">
+            <img src="../assets/Yugioh_anime_logo.webp" alt="">
+        </div>
+
+        <div class="title">
+            <h1>Yu-Gi-Oh API</h1>
+        </div>
+
+    </header>
+
+</template>
+
+<style scoped>
+header {
+    width: 100%;
+    display: flex;
+    height: 4rem;
+    background-color: white;
+    color: black;
+}
+
+.logo {
+    width: 5%;
+    height: 100%;
+    padding: 1rem;
+}
+
+.logo>img {
+    height: 100%;
+}
+
+.title {
+    width: 20%;
+    height: 100%;
+    align-content: center;
+    padding-left: 1rem;
+}
+</style>
